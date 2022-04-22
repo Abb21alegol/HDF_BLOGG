727fffd9d018d6bf9ac69bc173e2d5987a88903a
Abb21alegol <92145506+Abb21alegol@users.noreply.github.com>
2022-04-22 17:59:41 +0200

    yes

---
+++ pages/newsletter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <div class="flex flex-wrap justify-center text-white place-content-center w-screen">
+    <div @click="goToDish(dish.id)" class="w-80 m-4" v-for="dish in dishes" :key="dish.id">
+      <DishListItem :item="dish"></DishListItem>
+    </div>
+  </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  async asyncData({$content}) {
+    let dishes = await $content('dishes').without(['body']).fetch()
+    return {
+      dishes
+    }
+  },
+  methods: {
+    goToDish(id) {
+      this.$router.push('/dish-details/'+id)
+    }
+  }
+}
+</script>
+
